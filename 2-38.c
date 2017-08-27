2781d295242dfe8e3157e6ac1a16665005f573ed
asscriz <31381688+asscriz@users.noreply.github.com>
2017-08-27 16:27:04 +0700

    Initial commit

---
+++ 2-38.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("58224090131\n");
+	printf("%-8s %-13s\n","Gunchanick","Wiriyalai");
+	printf("13/August/1995\n");
+	printf("%119s\n","http://www.npru.ac.th");
+	printf("%65s\n","bye bye");
+	return 0;
+}
